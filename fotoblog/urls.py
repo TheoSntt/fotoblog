1867d75790bd0b46e657b479295a4695b7d81924
Théo Sinatti <sinatti.theo@gmail.com>
2023-04-17 17:29:06 +0200

    Finish course part 1

--- fotoblog/urls.py
+++ fotoblog/urls.py
@@ -19,10 +19,27 @@ from django.urls import path
 
 import authentication.views
 import blog.views
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', authentication.views.login_page, name='login'),
-    path('logout/', authentication.views.logout_user, name='logout'),
+    path('',
+        #  authentication.views.LoginPageView.as_view(), name='login'),
+        LoginView.as_view(
+            template_name='authentication/login.html',
+            redirect_authenticated_user=True),
+            name='login'),
+        
+    # path('logout/', authentication.views.logout_user, name='logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('change-password/', PasswordChangeView.as_view(
+        template_name='authentication/password_change_form.html'),
+         name='password_change'
+         ),
+    path('change-password-done/', PasswordChangeDoneView.as_view(
+        template_name='authentication/password_change_done.html'),
+         name='password_change_done'
+         ),
     path('home/', blog.views.home, name='home'),
+    path('signup/', authentication.views.signup_page, name='signup'),
 ]
